915445659c3f981b0dc135b3509f1607257d043d
Arvind8998 <asangwan8998@gmail.com>
2018-07-14 22:49:16 +0530

    initial

--- models/user.js
+++ models/user.js
@@ -1,9 +1,259 @@
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs');
+// const config = require('../config/database');
+
+
+
+
+
+// const UserSchema = mongoose.Schema({
+
+//   email: {
+//     type: String,
+//     required: true
+//   },
+//   username: {
+//     type: String,
+//     required: true,
+    
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   },
+  
+//   institutename: {
+//     type: String,
+//     required: true,
+    
+//   },
+//   address: {
+//     type: String,
+//      required: true
+//   },
+//   mobile: {
+//     type: Number,
+//     required: true,
+//     min : 0,
+//     max : 10,
+ 
+//   }
+//   // batch : [BatchSchema]
+// });
+// const BatchSchema =  mongoose.Schema({
+
+//   batchno : { 
+//     type : Number ,
+//     min : 0,
+//     max : 5 ,
+//     required : true
+//   },
+ 
+
+//   days : [String],
+
+//   // time : {
+//   //    type : Date 
+//   //    "default" : Date.now
+//   //  },
+//   batchno : { 
+//     type : Number ,
+//     min : 0,
+//     max : 5 ,
+//     required : true
+//   },
+
+//   days : [String],
+
+//   time : {
+//     type : Date ,
+//     "default" : Date.now
+//   },
+
+//   medium: {
+//     type: String,
+//     required: true
+//   },
+
+//   Subname : {
+//     type: [String]
+//   },
+
+//   Classname : {
+//     type: [String]
+//   }
+ 
+// });
+
+// const InstituteSubjectSchema = mongoose.Schema({
+
+//   Subname : {
+//     type: String
+//   },
+  
+ 
+// });
+
+// const InstituteClassSchema = mongoose.Schema({
+   
+//   Classname : {
+//     type: String
+//   },
+
+//   Subject : [InstituteSubjectSchema],
+//   batch : [BatchSchema ] 
+
+// });
+
+
+// const UserSchema = mongoose.Schema({
+//   name: {
+//     type: String
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     // unique: true
+//   },
+//   username: {
+//     type: String,
+//     required: true,
+    // unique: true
+  // },
+  
+  //sub : [String], 
+
+  // password: {
+  //   type: String,
+  //   required: true
+  // }, 
+
+  // institutename: {
+  //       type: String,
+  //       required: true,
+        
+  //     },
+  //     address: {
+  //       type: String,
+  //        required: true
+  //     },
+  //     mobile: {
+  //       type: String,
+  //       required: true
+      //   min : 0,
+      //   max : 10
+     
+      // }
+  // batch : [BatchSchema ]
+  // Class : [InstituteClassSchema]
+
+// });
+
+
+
+
+
+
+// const User = module.exports = mongoose.model('User', UserSchema);
+
+// module.exports.userGetOne = function(req, res) {
+//   var id = req.params.userId;
+//   console.log('GET userId', id);
+
+//  User
+//     .findById(id)
+//     .exec(function(err, doc) {
+//       res
+//         .status(200)
+//         .json(doc);
+//     });
+
+// };
+
+
+// module.exports.getUserById = function(id, callback){
+//   User.findById(id, callback);
+// }
+// module.exports.getSubById = function(id, callback){
+//   User.findById(id, callback);
+// }
+
+// module.exports.getUserByUsername = function(username, callback){
+//   const query = {username: username}
+//   User.findOne(query, callback);
+// }
+// module.exports.getAllUsers = function(callback){
+//   User.find(callback);
+// }
+// module.exports.addUser = function(newUser, callback){
+//   bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(newUser.password, salt, (err, hash) => {
+//       if(err) throw err;
+//       newUser.password = hash;
+//       newUser.save(callback);
+//     });
+//   });
+// }
+
+// module.exports.comparePassword = function(candidatePassword, hash, callback){
+//   bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+//     if(err) throw err;
+//     callback(null, isMatch);
+//   });
+// }
+
+// module.exports.usergetone = function (req , res) {
+
+//   var userId = req.params.userid;
+//   console.log("get usereid" , userid);
+  
+// User
+//   .findById(userid)
+//   .select('batch')
+//   .exec(function(err , doc) {
+//     console.log("Returned doc" , doc);
+//    res
+
+//   .status(200)
+//   .json(doc.batch);
+//   });
+
+// };
+// module.exports.getUserBatch = function(id, callback){
+//   User.findById(id, callback).select('batch');
+// }
+// module.exports.getUsermBatch = function(id, callback){
+//   User.findById(id, callback).select('batch').select('num');
+// }
+// module.exports.addBatch = function(user, req, callback){
+//   user.batch.push({
+//     batchno : req.body.batchno,
+//     days: req.body.days,
+//     time: req.body.time,
+//     medium: req.body.medium
+//  });
+//  user.save(callback);
+// }
+
+// module.exports.addMBatch = function(user, req, callback){
+//   user.batch.push({
+//     batchno : req.body.batchno,
+//     days: req.body.days,
+//     time: req.body.time,
+//     medium: req.body.medium,
+//     Subname :req.body.Subname,
+//     Classname : req.body.Classname
+    
+//  });
+//  user.save(callback);
+// }
+
+
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const config = require('../config/database');
 
-// User Schema
-
+//batch Scher,a
 const BatchSchema =  mongoose.Schema({
 
   batchno : { 
@@ -12,62 +262,54 @@ const BatchSchema =  mongoose.Schema({
     max : 5 ,
     required : true
   },
-  subject : {
-    type : String
-  },
 
   days : [String],
 
-  // time : {
-    //  type : Date 
-    //  "default" : Date.now
-  //  },
+  time : {
+    type : Date ,
+    "default" : Date.now
+  },
 
- 
+  medium: {
+    type: String,
+    required: true
+  }
 });
 
-
+// User Schema
 const UserSchema = mongoose.Schema({
   name: {
     type: String
   },
   email: {
     type: String,
-    required: true,
+    required: true, 
     unique: true
   },
   username: {
     type: String,
-    required: true,
-    unique: true
+    required: true
   },
   password: {
     type: String,
     required: true
-  }, 
-  batch: [BatchSchema]
-
+  },
+  batch: [BatchSchema],
+  institutename: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+     required: true
+  },
+  mobile: {
+    type: Number,
+    required: true
+  }
 });
 
-
-
-
-
 const User = module.exports = mongoose.model('User', UserSchema);
-
-module.exports.userGetOne = function(req, res) {
-  var id = req.params.userId;
-  console.log('GET userId', id);
-
- User
-    .findById(id)
-    .exec(function(err, doc) {
-      res
-        .status(200)
-        .json(doc);
-    });
-
-};
 
 
 module.exports.getUserById = function(id, callback){
@@ -75,12 +317,14 @@ module.exports.getUserById = function(id, callback){
 }
 
 module.exports.getUserByUsername = function(username, callback){
-  const query = {username: username}
+  const query = {username: username};
   User.findOne(query, callback);
 }
+
 module.exports.getAllUsers = function(callback){
   User.find(callback);
 }
+
 module.exports.addUser = function(newUser, callback){
   bcrypt.genSalt(10, (err, salt) => {
     bcrypt.hash(newUser.password, salt, (err, hash) => {
@@ -97,21 +341,15 @@ module.exports.comparePassword = function(candidatePassword, hash, callback){
     callback(null, isMatch);
   });
 }
-
-module.exports.usergetone = function (req , res) {
-
-  var userId = req.params.userid;
-  console.log("get usereid" , userid);
-  
-User
-  .findById(userid)
-  .select('batch')
-  .exec(function(err , doc) {
-    console.log("Returned doc" , doc);
-   res
-
-  .status(200)
-  .json(doc.batch);
-  });
-
-};
+module.exports.getUserBatch = function(id, callback){
+  User.findById(id, callback).select('batch');
+}
+module.exports.addBatch = function(user, req, callback){
+  user.batch.push({
+    batchno : req.body.batchno,
+    days: req.body.days,
+    time: req.body.time,
+    medium: req.body.medium
+ });
+ user.save(callback);
+}
--- routes/users.js
+++ routes/users.js
@@ -1,161 +1,303 @@
+// const express = require('express');
+// const router = express.Router();
+// const passport = require('passport');
+// const jwt = require('jsonwebtoken');
+// const config = require('../config/database');
+// const User = require('../models/user');
+
+
+//get all user
+// router
+//   .get('/user', function(req,res) {
+   
+//     User
+//     .find( {} , function(err, users){
+ 
+//  if (err) {
+//   console.log("error finding user");
+//   next();
+// }     
+//  res
+//        .json(users);
+//      });
+//     });
+    
+
+
+
+//     router.get('/getall', (req, res, next) => {
+//       User.getAllUsers((err, user)=>{
+//         if(err) throw err;
+//         if(user){
+//           var newusr = [];
+//           for (usr of user){
+//               newusr.push({id: usr._id,
+//                 name: usr.name,
+//                 username: usr.username,
+//                 email: usr.email}
+//               );
+//           }
+//           res.json({success: true,
+//             user: newusr
+//           });
+//         }
+//       });
+//     });
+
+     //get single user
+// router
+// .get('/user/:userid', (req, res, next) => {
+//   const userid = req.params.userid;
+//   console.log("hi");
+//   User
+//    .findById(userid)
+//   .exec(function(err , doc) {
+
+//       var response = {
+//           status : 200,
+//           message : doc
+//       };
+      
+//   if (err) {
+//       console.log("error finding user");         
+//               response.status = 500;
+//               response.message = err;
+//           }
+
+//           else if(!doc)
+//               {       
+//                 response.status = 404;
+//                       response . message = {
+//                           "message" : " user id not found"};
+//                       }
+                      
+//          res
+
+//          .status(response.status)
+//          .json(response.message);
+//      });
+//     });
+// router
+// .get('/sub', (req, res, next) => {
+//   var subId = "5b365c0d66d9804db60695a9";
+//   console.log("hi");
+//   User
+//    .findById(subId)
+//   .exec(function(err , doc) {
+// console.log(doc.username);
+//       var response = {
+//           status : 200,
+//           message : doc.username
+//       };
+      
+//   if (err) {
+//       console.log("error finding user");         
+//               response.status = 500;
+//               response.message = err;
+//           }
+
+//           else if(!doc)
+//               {       
+//                 response.status = 404;
+//                       response . message = {
+//                           "message" : " user id not found"};
+//                       }
+                      
+//          res
+
+//          .status(response.status)
+//          .json(response.message);
+//      });
+//     });
+    
+    // add a batch
+   
+    // var addBatch = function(req,res,user){
+    
+    //   user.batch.push({
+
+    //    batchno : parseInt(req.body.batchno , 10),
+    //    subject : req.body.subject,
+    //    days : req.body.days,
+      //  time : new Date (req.body.time)
+        
+    //   });
+    //  user.save(function(err , userUpdated){
+
+    //   if(err){
+    //     res
+    //     .status(500)
+    //     .json(err);
+    //   }
+    //   else{
+    //     res
+    //     .status(201)
+    //     .json(userUpdated.batch.length - 1)
+    //   }
+    //  });
+    // };
+
+    // router
+    // .post
+    // ('/user/:userid/batches', (req, res, next) => {
+    //   var userid = req.params.userid;
+    //   console.log('GET userId', userid);
+    
+    //  User
+    //     .findById(userid)
+    //     .select('batch')
+    //     .exec(function(err, doc) {
+    //       var response = {
+    //         status : 200,
+    //         message : doc
+    //     };
+    // if (err) {
+    //     console.log("error finding user");
+            
+    //             response.status = 500;
+    //             response.message = err;
+    //         }
+  
+    //         else if(!doc)
+    //             {
+    //                     response.status = 404;
+    //                     response . message = {
+    //                         "message" : " user id not found"};
+    //                     }
+    //        if(doc){
+    //          addBatch(req,res,doc);
+    //        }             
+    //        res
+  
+    //        .status(response.status)
+    //        .json(response.message);
+           
+    //    });
+    // });
+    
+    // router.post('/:userId/batch', (req, res, next) => {
+    //   const userId = req.params.userId;
+    //   User.getUsermBatch(userId,(err, user)=>{
+    //     if(err)throw err;
+    //     else if(!user){
+    //       res.json({success: false,
+    //       msg: "No userId with id = " + userId});
+    //     }
+    //    if(user){
+    //     User.addBatch(user, req ,(err, batch) => {
+    //       if(err) throw err;
+    //       if(batch){
+    //         res.json({
+    //           success: true,
+    //           msg: "Batch Updated",
+    //           batch: batch
+    //         });
+    //       }
+    //     });
+    //    }
+    //   });
+    // });
+    
+
+// Register
+// router
+// .post('/register', (req, res, next) => {
+//   let newUser = new User({
+//     name :  req.body.name,
+//     email: req.body.email,
+//     username: req.body.username,
+//     password: req.body.password,
+//     address: req.body.address,
+//     institutename: req.body.institutename,
+//     mobile: req.body.mobile
+    
+//   });
+
+//   User
+//     .addUser(newUser, (err, user) => {
+//     if(err){
+//       console.log("err");
+//       res.json({success: false, msg:'Failed to register user'});
+//     } else {
+//       console.log("success");
+//       res.json({success: true, msg:'User registered'});
+//     }
+//   });
+// });
+
+// // Authenticate
+// router
+// .post('/authenticate', (req, res, next) => {
+//   const username = req.body.username;
+//   const password = req.body.password;
+
+//   User
+//   .getUserByUsername(username, (err, user) => {
+//     if(err) throw err;
+//     if(!user){
+//       return res.json({success: false, msg: 'User not found'});
+//     }
+
+//     User
+//     .comparePassword(password, user.password, (err, isMatch) => {
+//       if(err) throw err;
+//       if(isMatch){
+//         const token = jwt.sign(user.toJSON(), config.secret, {
+//           expiresIn: 604800 // 1 week
+//         });
+
+//         res.json({
+//           success: true,
+//           token: 'JWT '+token,
+//           user: {
+//             id: user._id,
+//             name: user.name,
+//             username: user.username,
+//             email: user.email
+//           }
+//         });
+//       } else {
+//         return res.json({success: false, msg: 'Wrong password'});
+//       }
+//     });
+//   });
+// });
+
+//batch
+
+
+
+ 
+// Profile
+// router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
+//   res.json({user: req.user});
+// });
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const config = require('../config/database');
 const User = require('../models/user');
+const Batch = require('../models/user');
 
-
-//get all user
-router
-  .get('/user', function(req,res) {
-   
-    User
-    .find( {} , function(err, users){
- 
- if (err) {
-  console.log("error finding user");
-  next();
-}     
- res
-       .json(users);
-     });
-    });
-    
-
-
-
-    router.get('/getall', (req, res, next) => {
-      User.getAllUsers((err, user)=>{
-        if(err) throw err;
-        if(user){
-          var newusr = [];
-          for (usr of user){
-              newusr.push({id: usr._id,
-                name: usr.name,
-                username: usr.username,
-                email: usr.email}
-              );
-          }
-          res.json({success: true,
-            user: newusr
-          });
-        }
-      });
-    });
-
-     //get single user
-router
-.get('/user/:userid', (req, res, next) => {
-  const userid = req.params.userid;
-  console.log("hi");
-  User
-   .findById(userid)
-  .exec(function(err , doc) {
-
-      var response = {
-          status : 200,
-          message : doc
-      };
-      
-  if (err) {
-      console.log("error finding user");         
-              response.status = 500;
-              response.message = err;
-          }
-
-          else if(!doc)
-              {       
-                response.status = 404;
-                      response . message = {
-                          "message" : " user id not found"};
-                      }
-                      
-         res
-
-         .status(response.status)
-         .json(response.message);
-     });
-    });
-    
-    // add a batch
-   
-    var addBatch = function(req,res,user){
-    
-      user.batch.push({
-
-       batchno : parseInt(req.body.batchno , 10),
-       subject : req.body.subject,
-       days : req.body.days,
-       time : new Date (req.body.time)
-        
-      });
-     user.save(function(err , userUpdated){
-
-      if(err){
-        res
-        .status(500)
-        .json(err);
-      }
-      else{
-        res
-        .status(201)
-        .json(userUpdated.batch.length - 1)
-      }
-     });
-    };
-
-    router
-    .post
-    ('/user/:userid/batches', (req, res, next) => {
-      var userid = req.params.userid;
-      console.log('GET userId', userid);
-    
-     User
-        .findById(userid)
-        .exec(function(err, doc) {
-          var response = {
-            status : 200,
-            message : doc
-        };
-    if (err) {
-        console.log("error finding user");
-            
-                response.status = 500;
-                response.message = err;
-            }
-  
-            else if(!doc)
-                {
-                        response.status = 404;
-                        response . message = {
-                            "message" : " user id not found"};
-                        }
-           if(doc){
-             addBatch(req,res,doc);
-           }             
-           res
-  
-           .status(response.status)
-           .json(response.message);
-       });
-    });
-    
 // Register
-router
-.post('/register', (req, res, next) => {
+router.post('/register', (req, res, next) => {
   let newUser = new User({
     name: req.body.name,
     email: req.body.email,
     username: req.body.username,
     password: req.body.password,
-    
+    address: req.body.address,
+    institutename: req.body.institutename,
+    mobile: req.body.mobile
   });
 
-  User
-    .addUser(newUser, (err, user) => {
+  User.addUser(newUser, (err, user) => {
     if(err){
-      res.json({success: false, msg:'Failed to register user'});
+      res.json({success: false, msg: err+'Failed to register user'});
     } else {
       res.json({success: true, msg:'User registered'});
     }
@@ -163,20 +305,17 @@ router
 });
 
 // Authenticate
-router
-.post('/authenticate', (req, res, next) => {
+router.post('/authenticate', (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  User
-  .getUserByUsername(username, (err, user) => {
+  User.getUserByUsername(username, (err, user) => {
     if(err) throw err;
     if(!user){
       return res.json({success: false, msg: 'User not found'});
     }
 
-    User
-    .comparePassword(password, user.password, (err, isMatch) => {
+    User.comparePassword(password, user.password, (err, isMatch) => {
       if(err) throw err;
       if(isMatch){
         const token = jwt.sign(user.toJSON(), config.secret, {
@@ -190,7 +329,10 @@ router
             id: user._id,
             name: user.name,
             username: user.username,
-            email: user.email
+            email: user.email,
+            address: user.address,
+            institutename: user.institutename,
+            mobile: user.mobile
           }
         });
       } else {
@@ -200,14 +342,70 @@ router
   });
 });
 
-//batch
-
-
-
- 
 // Profile
 router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
   res.json({user: req.user});
 });
+
+
+router.get('/getall', (req, res, next) => {
+User.getAllUsers((err, user)=>{
+  if(err) throw err;
+  if(user){
+    var newusr = [];
+    for (usr of user){
+        newusr.push({id: usr._id,
+          name: usr.name,
+          username: usr.username,
+          email: usr.email,
+          address: usr.address,
+          institutename: usr.institutename,
+          mobile: usr.mobile,
+          batch: usr.batch
+        }
+        );
+    }
+    res.json({success: true,
+      user: newusr
+    });
+  }
+});
+});
+
+router.post('/:userId/batch', (req, res, next) => {
+  const userId = req.params.userId;
+  User.getUserBatch(userId,(err, user)=>{
+    if(err)throw err;
+    else if(!user){
+      res.json({success: false,
+      msg: "No userId with id = " + userId});
+    }
+   if(user){
+    User.addBatch(user, req ,(err, batch) => {
+      if(err) throw err;
+      if(batch){
+        res.json({
+          success: true,
+          msg: "Batch Updated",
+          batch: batch
+        });
+      }
+    });
+   }
+  });
+});
+
+router.get('/:userId', (req, res) => {
+  const userId = req.params.userId;
+  User.getUserById(userId, (err, user)=>{
+    if(err) throw err;
+    else if(user){
+      res.json({
+        success: true,
+        user: user
+      })
+    }
+  })
+})
 
 module.exports = router;
